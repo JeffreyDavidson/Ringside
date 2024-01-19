538184bc8e303056fa00cf618480bdced0ae8550
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-19 11:10:37 -0500

    improve fomratting

--- resources/views/livewire/wrestlers/wrestlers-list.blade.php
+++ resources/views/livewire/wrestlers/wrestlers-list.blade.php
@@ -1,31 +1,46 @@
-
 <x-card>
     <x-slot name="header">
         @include('livewire.wrestlers.partials.header')
     </x-slot>
 
-    <x-card.body class="pt-0">
+    <x-card.body class="py-4">
         <div class="table-responsive">
             <x-table class="table-row-dashed fs-6 gy-5 dataTable no-footer">
                 <x-slot name="head">
-                    <x-table.heading class="w-10px pe-2 sorting_disabled"><x-form.inputs.checkbox wire:model="selectPage" /></x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('name')" :direction="$sorts['name'] ?? null" class="min-w-125px sorting">Wrestler Name</x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('status')" :direction="$sorts['status'] ?? null" class="min-w-125px sorting">Status</x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('height')" :direction="$sorts['height'] ?? null" class="min-w-125px sorting">Height</x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('weight')" :direction="$sorts['weight'] ?? null" class="min-w-125px sorting">Weight</x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('hometown')" :direction="$sorts['hometown'] ?? null" class="min-w-125px sorting">Hometown</x-table.heading>
-                    <x-table.heading sortable multi-column wire:click="sortBy('hometown')" :direction="$sorts['start_date'] ?? null" class="min-w-125px sorting">Start Date</x-table.heading>
+                    <x-table.heading class="w-10px pe-2 sorting_disabled">
+                        <x-form.inputs.checkbox wire:model="selectPage"/>
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('name')"
+                                     :direction="$sorts['name'] ?? null" class="min-w-125px sorting">Wrestler Name
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('status')"
+                                     :direction="$sorts['status'] ?? null" class="min-w-125px sorting">Status
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('height')"
+                                     :direction="$sorts['height'] ?? null" class="min-w-125px sorting">Height
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('weight')"
+                                     :direction="$sorts['weight'] ?? null" class="min-w-125px sorting">Weight
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('hometown')"
+                                     :direction="$sorts['hometown'] ?? null" class="min-w-125px sorting">Hometown
+                    </x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('hometown')"
+                                     :direction="$sorts['start_date'] ?? null" class="min-w-125px sorting">Start Date
+                    </x-table.heading>
                     <x-table.heading class="text-end min-w-70px sorting_disabled">Actions</x-table.heading>
                 </x-slot>
                 <x-slot name="body">
                     @forelse ($wrestlers as $wrestler)
-                        <x-table.row :class="$loop->odd ? 'odd' : 'even'" wire:loading.class.delay="opacity-50" wire:key="row-{{ $wrestler->id }}">
+                        <x-table.row :class="$loop->odd ? 'odd' : 'even'" wire:loading.class.delay="opacity-50"
+                                     wire:key="row-{{ $wrestler->id }}">
                             <x-table.cell>
-                                <x-form.inputs.checkbox wire:model="selected" value="{{ $wrestler->id }}" />
+                                <x-form.inputs.checkbox wire:model="selected" value="{{ $wrestler->id }}"/>
                             </x-table.cell>
 
                             <x-table.cell>
-                                <a class="text-gray-800 text-hover-primary" href="{{ route('wrestlers.show', $wrestler) }}">{{ $wrestler->name }}</a>
+                                <a class="text-gray-800 text-hover-primary"
+                                   href="{{ route('wrestlers.show', $wrestler) }}">{{ $wrestler->name }}</a>
                             </x-table.cell>
 
                             <x-table.cell>
@@ -59,7 +74,8 @@
                         <x-table.row>
                             <x-table.cell colspan="6">
                                 <div class="flex items-center justify-center space-x-2">
-                                    <span class="py-8 text-xl font-medium text-cool-gray-400">No wrestlers found...</span>
+                                    <span
+                                        class="py-8 text-xl font-medium text-cool-gray-400">No wrestlers found...</span>
                                 </div>
                             </x-table.cell>
                         </x-table.row>
@@ -69,7 +85,8 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start"></div>
+            <div
+                class="col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start"></div>
             <div class="col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end">
                 {{ $wrestlers->links() }}
             </div>
