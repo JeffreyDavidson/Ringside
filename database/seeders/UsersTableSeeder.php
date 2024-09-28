54d04c05b177721f5a1f4f00472aadb1ec865d3c
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-28 10:56:18 -0400

    Cleanup user seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,23 +15,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $userData = [];
-        collect(Role::cases())->each(function ($role, $key) use (&$userData): void {
+        collect(Role::cases())->each(function ($role, $key): void {
             for ($i = 0; $i <= 1; $i++) {
-                $user = new User;
-                $user->first_name = ($i ? 'Second ' : '').ucwords($role->value);
-                $user->last_name = 'User';
-                $user->email = $role->value.($i ? '2' : '').'@example.com';
-                $user->password = 'password';
-                $user->role = $role->value;
-                $user->save();
-                $user->refresh();
-                $userData[] = [
-                    'role' => $role->value,
-                    'name' => $user->first_name.' '.$user->last_name,
-                    'email' => $user->email,
+                User::query()->firstOrNew([
+                    'first_name' => ($i ? 'Second ' : '').ucwords($role->value),
+                    'last_name' => 'User',
+                    'email' => $role->value.($i ? '2' : '').'@example.com',
                     'password' => 'password',
-                ];
+                    'role' => $role->value
+                ]);
             }
         });
     }
