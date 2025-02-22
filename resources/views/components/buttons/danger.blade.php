197b6d15c9c4be3f2518c0da1735e6e2fe100650
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-02-22 11:28:00 -0500

    Add changes to buttons

--- resources/views/components/buttons/danger.blade.php
+++ resources/views/components/buttons/danger.blade.php
@@ -1,16 +1,18 @@
 @props([
-    'size' => null,
-    'outline' => false,
-    'withIcon' => false,
-    'iconOnly' => false,
-    'clear' => false,
-    'disabled' => false,
+    'size' => 'default',
+    'variant' => 'default',
 ])
 
 <x-button
     {{ $attributes->merge()->class([
-            'text-white bg-danger hover:bg-danger-active hover:shadow-danger-box-shadow active:bg-danger-active active:shadow-danger-box-shadow focus:bg-danger-active focus:shadow-danger-box-shadow',
-        ]) }}
-    :$size :$outline :$withIcon :$iconOnly :$clear :$disabled>
+        'bg-danger-light' => $variant === 'outline',
+        'bg-danger' => $variant === 'default',
+        'border border-solid border-danger-clarity' => $variant !== 'clear',
+        'text-white hover:bg-danger-active hover:shadow hover:shadow-danger active:bg-danger-active active:shadow active:shadow-danger focus:bg-danger-active focus:shadow focus:shadow-danger' =>
+            $variant === 'default',
+        'text-danger hover:bg-danger hover:shadow-none hover:text-danger-inverse hover:border-danger active:text-danger-inverse active:bg-danger active:border-danger active:shadow-none focus:text-danger-inverse focus:bg-danger focus:border-danger focus:shadow-none' =>
+            $variant !== 'default',
+    ]) }}
+    :$size>
     {{ $slot }}
 </x-button>
