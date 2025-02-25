da69635c248f591d93a3d5a27f2c85fcc24ab2d4
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-02-24 20:13:20 -0500

    Install alpine ui plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+import Clipboard from '@ryangjchandler/alpine-clipboard';
+import AlpineUI from '@alpinejs/ui';
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
 import '../vendors/keenicons/styles.bundle.css';
 import '../css/app.css';
@@ -9,5 +10,6 @@ import.meta.glob([
     '../images/**',
 ])
 
-Alpine.plugin(Clipboard)
+Alpine.plugin(Clipboard);
+Alpine.plugin(AlpineUI);
 Livewire.start();
