f769444d9ec0faf08161d2ef165fd54da44d9d1e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-03 10:56:42 -0400

    Add validation error messages

--- lang/en/employments.php
+++ lang/en/employments.php
@@ -2,4 +2,8 @@
 
 return [
     'started_at' => 'Start Date',
+
+    'validation' => [
+        'employment_released' => ':name is currently activated and the activation date cannot be changed.',
+    ]
 ];
