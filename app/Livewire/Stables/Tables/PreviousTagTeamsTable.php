d0b4bb57da69f009a5fb7c8a5d665b7056c8b905
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-06 13:27:20 -0400

    Refactor to a base table component

--- app/Livewire/Stables/Tables/PreviousTagTeamsTable.php
+++ app/Livewire/Stables/Tables/PreviousTagTeamsTable.php
@@ -4,22 +4,13 @@ declare(strict_types=1);
 
 namespace App\Livewire\Stables\Tables;
 
-use App\Livewire\Concerns\ShowTableTrait;
+use App\Livewire\Base\Tables\BasePreviousTagTeamsTable;
 use App\Models\StableTagTeam;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Rappasoft\LaravelLivewireTables\DataTableComponent;
-use Rappasoft\LaravelLivewireTables\Views\Column;
-use Rappasoft\LaravelLivewireTables\Views\Columns\DateColumn;
-use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
 
-class PreviousTagTeamsTable extends DataTableComponent
+class PreviousTagTeamsTable extends BasePreviousTagTeamsTable
 {
-    use ShowTableTrait;
-
     protected string $databaseTableName = 'stables_tag_teams';
-
-    protected string $resourceName = 'tag teams';
 
     public ?int $stableId;
 
@@ -44,23 +35,5 @@ class PreviousTagTeamsTable extends DataTableComponent
         $this->addAdditionalSelects([
             'stables_tag_teams.tag_team_id as tag_team_id',
         ]);
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @return array<int, Column>
-     */
-    public function columns(): array
-    {
-        return [
-            LinkColumn::make(__('tag-teams.name'))
-                ->title(fn (Model $row) => $row->tagTeam->name)
-                ->location(fn (Model $row) => route('tag-teams.show', $row)),
-            DateColumn::make(__('stables.date_joined'), 'joined_at')
-                ->outputFormat('Y-m-d'),
-            DateColumn::make(__('stables.date_left'), 'left_at')
-                ->outputFormat('Y-m-d'),
-        ];
     }
 }
