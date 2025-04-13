bfe497275532397be00e01c3e6b29a951cf09429
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-13 10:35:09 -0400

    Add laravel rector

--- rector.php
+++ rector.php
@@ -3,10 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
+    ])
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
     ])
     ->withPreparedSets(
         deadCode: false,
