e9bcabea761cde3c22ceb1a7140612c169b1965c
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 10:56:18 -0400

    Cleanup routes file

--- routes/web.php
+++ routes/web.php
@@ -19,37 +19,29 @@ require __DIR__.'/auth.php';
 
 Route::redirect('/', 'login');
 
-Route::view('/test', 'test');
-
-Route::middleware(['middleware' => 'auth'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
-});
 
-Route::middleware(['auth'])->prefix('roster')->group(function () {
-    Route::resource('stables', StablesController::class)->only(['index', 'show']);
-    Route::resource('wrestlers', WrestlersController::class)->only(['index', 'show']);
-    Route::resource('managers', ManagersController::class)->only(['index', 'show']);
-    Route::resource('referees', RefereesController::class)->only(['index', 'show']);
-    Route::resource('tag-teams', TagTeamsController::class)->only(['index', 'show']);
-});
+    Route::prefix('roster')->group(function () {
+        Route::resource('stables', StablesController::class)->only(['index', 'show']);
+        Route::resource('wrestlers', WrestlersController::class)->only(['index', 'show']);
+        Route::resource('managers', ManagersController::class)->only(['index', 'show']);
+        Route::resource('referees', RefereesController::class)->only(['index', 'show']);
+        Route::resource('tag-teams', TagTeamsController::class)->only(['index', 'show']);
+    });
 
-Route::middleware(['auth'])->group(function () {
     Route::resource('titles', TitlesController::class)->only(['index', 'show']);
-});
 
-Route::middleware(['auth'])->group(function () {
     Route::get('events/{event}/matches', [EventMatchesController::class, 'index'])->name('events.matches');
     Route::resource('events', EventsController::class)->only(['index', 'show']);
-});
 
-Route::middleware(['auth'])->group(function () {
     Route::resource('venues', VenuesController::class)->only(['index', 'show']);
-});
 
-Route::middleware(['auth'])->prefix('user-management')->group(function () {
-    Route::resource('users', UsersController::class)->only(['index', 'show']);
-});
+    Route::prefix('user-management')->group(function () {
+        Route::resource('users', UsersController::class)->only(['index', 'show']);
+    });
 
-Route::prefix('docs')->group(function () {
-    Route::view('buttons', 'docs.buttons')->name('docs.buttons');
+    Route::prefix('docs')->group(function () {
+        Route::view('buttons', 'docs.buttons')->name('docs.buttons');
+    });
 });
