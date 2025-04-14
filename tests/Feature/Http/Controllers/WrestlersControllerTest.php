19ddd5d2f76ce4d6244be5da1d90dc050a2b9ef2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 12:20:50 -0400

    Move test cases to new test file

--- tests/Feature/Http/Controllers/WrestlersControllerTest.php
+++ tests/Feature/Http/Controllers/WrestlersControllerTest.php
@@ -2,6 +2,69 @@
 
 declare(strict_types=1);
 
-describe('index', function () {});
+use App\Http\Controllers\WrestlersController;
+use App\Livewire\Wrestlers\Tables\WrestlersTable;
+use App\Models\User;
+use App\Models\Wrestler;
 
-describe('show', function () {});
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+describe('index', function () {
+    test('index returns a view', function () {
+        actingAs(administrator())
+            ->get(action([WrestlersController::class, 'index']))
+            ->assertOk()
+            ->assertViewIs('wrestlers.index')
+            ->assertSeeLivewire(WrestlersTable::class);
+    });
+
+    test('a basic user cannot view wrestlers index page', function () {
+        actingAs(basicUser())
+            ->get(action([WrestlersController::class, 'index']))
+            ->assertForbidden()
+            ->assertDontSeeLivewire(WrestlersTable::class);
+    });
+
+    test('a guest cannot view wrestlers index page', function () {
+        get(action([WrestlersController::class, 'index']))
+            ->assertRedirect(route('login'));
+    });
+});
+
+describe('show', function () {
+    beforeEach(function () {
+        $this->wrestler = Wrestler::factory()->create();
+    });
+
+    test('show returns a view', function () {
+        actingAs(administrator())
+            ->get(action([WrestlersController::class, 'show'], $this->wrestler))
+            ->assertOk()
+            ->assertViewIs('wrestlers.show')
+            ->assertViewHas('wrestler', $this->wrestler);
+    });
+
+    test('a basic user can view their wrestler profile', function () {
+        $wrestler = Wrestler::factory()->for($user = basicUser())->create();
+
+        actingAs($user)
+            ->get(action([WrestlersController::class, 'show'], $wrestler))
+            ->assertOk()
+            ->assertViewIs('wrestlers.show')
+            ->assertViewHas('wrestler', $wrestler);
+    });
+
+    test('a basic user cannot view another users wrestler profile', function () {
+        $wrestler = Wrestler::factory()->for(User::factory())->create();
+
+        actingAs(basicUser())
+            ->get(action([WrestlersController::class, 'show'], $wrestler))
+            ->assertForbidden();
+    });
+
+    test('a guest cannot view a wrestler profile', function () {
+        get(action([WrestlersController::class, 'show'], $this->wrestler))
+            ->assertRedirect(route('login'));
+    });
+});
