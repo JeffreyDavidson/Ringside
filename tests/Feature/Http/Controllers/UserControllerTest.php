aa726d7de9fc650a20de53cd50d821fad0b8ea54
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 12:16:45 -0400

    Move test cases to new test file

--- tests/Feature/Http/Controllers/UserControllerTest.php
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -2,6 +2,53 @@
 
 declare(strict_types=1);
 
-describe('index', function () {});
+use App\Http\Controllers\UsersController;
+use App\Livewire\Users\Tables\UsersTable;
+use App\Models\User;
 
-describe('show', function () {});
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+describe('index', function () {
+    test('index returns a view', function () {
+        actingAs(administrator())
+            ->get(action([UsersController::class, 'index']))
+            ->assertOk()
+            ->assertViewIs('users.index')
+            ->assertSeeLivewire(UsersTable::class);
+    });
+
+    test('a basic user cannot view Users index page', function () {
+        actingAs(basicUser())
+            ->get(action([UsersController::class, 'index']))
+            ->assertForbidden()
+            ->assertDontSeeLivewire(UsersTable::class);
+    });
+
+    test('a guest cannot view users index page', function () {
+        get(action([UsersController::class, 'index']))
+            ->assertRedirect(route('login'));
+    });
+});
+
+describe('show', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+    });
+
+    test('show returns a view', function () {
+        actingAs(administrator())
+            ->get(action([UsersController::class, 'show'], $this->user))
+            ->assertOk()
+            ->assertViewIs('users.show')
+            ->assertViewHas('user', $this->user);
+    });
+
+    test('a basic user can view their user profile', function () {
+        actingAs($user = basicUser())
+            ->get(action([UsersController::class, 'show'], $user))
+            ->assertOk()
+            ->assertViewIs('users.show')
+            ->assertViewHas('user', $user);
+    });
+});
