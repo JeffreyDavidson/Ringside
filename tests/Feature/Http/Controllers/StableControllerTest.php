64d433e79aadf7ae3b99a5d28f7649c4d9233fd3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 11:44:12 -0400

    Refactor stable tests

--- tests/Feature/Http/Controllers/StableControllerTest.php
+++ tests/Feature/Http/Controllers/StableControllerTest.php
@@ -2,6 +2,51 @@
 
 declare(strict_types=1);
 
-describe('index', function () {});
+use App\Http\Controllers\StablesController;
+use App\Livewire\Stables\Tables\StablesTable;
+use App\Models\Stable;
 
-describe('show', function () {});
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+describe('index', function () {
+    test('index returns a view', function () {
+        actingAs(administrator())
+            ->get(action([StablesController::class, 'index']))
+            ->assertOk()
+            ->assertViewIs('stables.index')
+            ->assertSeeLivewire(StablesTable::class);
+    });
+
+    test('a basic user cannot view stables index page', function () {
+        actingAs(basicUser())
+            ->get(action([StablesController::class, 'index']))
+            ->assertForbidden();
+    });
+
+    test('a guest cannot view stables index page', function () {
+        get(action([StablesController::class, 'index']))
+            ->assertRedirect(route('login'));
+    });
+});
+
+describe('show', function () {
+    beforeEach(function () {
+        $this->stable = Stable::factory()->create();
+    });
+
+    test('show returns a view', function () {
+        actingAs(administrator())
+            ->get(action([StablesController::class, 'show'], $this->stable))
+            ->assertViewIs('stables.show')
+            ->assertViewHas('stable', $this->stable);
+    });
+
+    test('a basic user can view their stable profile', function () {
+        $stable = Stable::factory()->for($user = basicUser())->create();
+
+        actingAs($user)
+            ->get(action([StablesController::class, 'show'], $stable))
+            ->assertOk();
+    });
+});
