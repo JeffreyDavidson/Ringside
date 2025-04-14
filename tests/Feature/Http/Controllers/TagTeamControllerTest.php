9dd1ea56e808ffdd881a7a4cabf39086a6922440
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 12:08:50 -0400

    Move test cases to new test file

--- tests/Feature/Http/Controllers/TagTeamControllerTest.php
+++ tests/Feature/Http/Controllers/TagTeamControllerTest.php
@@ -2,6 +2,51 @@
 
 declare(strict_types=1);
 
-describe('index', function () {});
+use App\Http\Controllers\TagTeamsController;
+use App\Livewire\TagTeams\Tables\TagTeamsTable;
+use App\Models\TagTeam;
 
-describe('show', function () {});
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+describe('index', function () {
+    test('index returns a view', function () {
+        actingAs(administrator())
+            ->get(action([TagTeamsController::class, 'index']))
+            ->assertOk()
+            ->assertViewIs('tag-teams.index')
+            ->assertSeeLivewire(TagTeamsTable::class);
+    });
+
+    test('a basic user cannot view tag teams index page', function () {
+        actingAs(basicUser())
+            ->get(action([TagTeamsController::class, 'index']))
+            ->assertForbidden();
+    });
+
+    test('a guest cannot view tag teams index page', function () {
+        get(action([TagTeamsController::class, 'index']))
+            ->assertRedirect(route('login'));
+    });
+});
+
+describe('show', function () {
+    beforeEach(function () {
+        $this->tagTeam = TagTeam::factory()->create();
+    });
+
+    test('show returns a view', function () {
+        actingAs(administrator())
+            ->get(action([TagTeamsController::class, 'show'], $this->tagTeam))
+            ->assertViewIs('tag-teams.show')
+            ->assertViewHas('tagTeam', $this->tagTeam);
+    });
+
+    test('a basic user can view their tag team profile', function () {
+        $tagTeam = TagTeam::factory()->for($user = basicUser())->create();
+
+        actingAs($user)
+            ->get(action([TagTeamsController::class, 'show'], $tagTeam))
+            ->assertOk();
+    });
+});
