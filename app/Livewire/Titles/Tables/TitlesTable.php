4eb1247a1944ed0c86701f5fbcdc3e6b1a34ca15
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-14 10:51:56 -0400

    Move action controller logic to livewire component

--- app/Livewire/Titles/Tables/TitlesTable.php
+++ app/Livewire/Titles/Tables/TitlesTable.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Livewire\Titles\Tables;
 
+use App\Actions\Titles\ActivateAction;
+use App\Actions\Titles\DeactivateAction;
+use App\Actions\Titles\RestoreAction;
+use App\Actions\Titles\RetireAction;
+use App\Actions\Titles\UnretireAction;
 use App\Builders\TitleBuilder;
 use App\Enums\ActivationStatus;
+use App\Exceptions\CannotBeActivatedException;
+use App\Exceptions\CannotBeDeactivatedException;
+use App\Exceptions\CannotBeRetiredException;
+use App\Exceptions\CannotBeUnretiredException;
 use App\Livewire\Base\Tables\BaseTableWithActions;
 use App\Livewire\Concerns\Columns\HasStatusColumn;
 use App\Livewire\Concerns\Filters\HasStatusFilter;
 use App\Models\Title;
 use App\View\Columns\FirstActivationDateColumn;
 use App\View\Filters\FirstActivationFilter;
+use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\Views\Filter;
 
@@ -72,5 +84,84 @@ final class TitlesTable extends BaseTableWithActions
     public function delete(Title $title): void
     {
         $this->deleteModel($title);
+    }
+
+    public function activate(Title $title): RedirectResponse
+    {
+        Gate::authorize('activate', $title);
+
+        try {
+            resolve(ActivateAction::class)->handle($title);
+        } catch (CannotBeActivatedException $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return back();
+    }
+
+    /**
+     * Deactivates a title.
+     */
+    public function deactivate(Title $title): RedirectResponse
+    {
+        Gate::authorize('deactivate', $title);
+
+        try {
+            resolve(DeactivateAction::class)->handle($title);
+        } catch (CannotBeDeactivatedException $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return back();
+    }
+
+    /**
+     * Restores a title.
+     */
+    public function restore(int $titleId): RedirectResponse
+    {
+        $title = Title::onlyTrashed()->findOrFail($titleId);
+
+        Gate::authorize('restore', $title);
+
+        try {
+            resolve(RestoreAction::class)->handle($title);
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return back();
+    }
+
+    /**
+     * Retires a title.
+     */
+    public function retire(Title $title): RedirectResponse
+    {
+        Gate::authorize('retire', $title);
+
+        try {
+            resolve(RetireAction::class)->handle($title);
+        } catch (CannotBeRetiredException $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return back();
+    }
+
+    /**
+     * Unretire a title.
+     */
+    public function unretire(Title $title): RedirectResponse
+    {
+        Gate::authorize('unretire', $title);
+
+        try {
+            resolve(UnretireAction::class)->handle($title);
+        } catch (CannotBeUnretiredException $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return back();
     }
 }
