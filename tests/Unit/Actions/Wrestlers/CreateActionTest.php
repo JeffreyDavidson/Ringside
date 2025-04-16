4eb3f72b46647cd3da372cedcaff245e41827547
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-16 18:32:28 -0400

    Fix tests (#460)

---
+++ tests/Unit/Actions/Wrestlers/CreateActionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\Wrestlers\CreateAction;
+use App\Data\WrestlerData;
+use App\Models\Wrestler;
+use App\Repositories\WrestlerRepository;
+
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    testTime()->freeze();
+
+    $this->wrestlerRepository = $this->mock(WrestlerRepository::class);
+});
+
+test('it creates a wrestler', function () {
+    $data = new WrestlerData(
+        'Example Wrestler Name',
+        70,
+        220,
+        'New York City, New York',
+        null,
+        null
+    );
+
+    $this->wrestlerRepository
+        ->shouldReceive('create')
+        ->once()
+        ->with($data)
+        ->andReturns(new Wrestler());
+
+    $this->wrestlerRepository
+        ->shouldNotReceive('employ');
+
+    resolve(CreateAction::class)->handle($data);
+});
+
+test('it employs a wrestler if start date is filled in request', function () {
+    $dateTime = now();
+    $data = new WrestlerData('Example Wrestler Name', 70, 220, 'New York City, New York', null, $dateTime);
+    $wrestler = Wrestler::factory()->create([
+        'name' => $data->name,
+        'height' => $data->height,
+        'weight' => 220,
+        'hometown' => $data->hometown,
+    ]);
+
+    $this->wrestlerRepository
+        ->shouldReceive('create')
+        ->once()
+        ->with($data)
+        ->andReturn($wrestler);
+
+    $this->wrestlerRepository
+        ->shouldReceive('employ')
+        ->once()
+        ->with($wrestler, $data->start_date);
+
+    resolve(CreateAction::class)->handle($data);
+});
---
+++ tests/Unit/Actions/Wrestlers/DeleteActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\Wrestlers\DeleteAction;
+use App\Models\Wrestler;
+use App\Repositories\WrestlerRepository;
+
+beforeEach(function () {
+    $this->wrestlerRepository = $this->mock(WrestlerRepository::class);
+});
+
+test('it deletes a wrestler', function () {
+    $wrestler = Wrestler::factory()->create();
+
+    $this->wrestlerRepository
+        ->shouldReceive('delete')
+        ->once()
+        ->with($wrestler);
+
+    resolve(DeleteAction::class)->handle($wrestler);
+});
