d908c27b490c0c7d15087c5bea60ec1a7bee7dcf
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-08-02 19:36:23 -0400

    feat: consolidate menu components and remove unused mega-menu system
    **Major Simplifications:**
    - Remove entire mega-menu directory (14 unused components)
    - Consolidate menu-item components with variant support
    - Update sidebar to use unified menu-item component
    
    **Removed Components:**
    - mega-menu/ directory (complete removal - 14 components)
    - sidebar/menu-item.blade.php (replaced with menu/menu-item.blade.php)
    
    **Enhanced Components:**
    - menu/menu-item.blade.php: Added variant prop for sidebar styling
    - Supports both default and sidebar variants with group class
    
    **Updated Usage:**
    - sidebar/index.blade.php: Updated to use x-menu.menu-item with variant="sidebar"
    
    **Impact:**
    - Reduced total menu components from 39 to 24 (-38% reduction)
    - Eliminated unused navigation system
    - Maintained all existing functionality
    - Improved component architecture consistency

--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -19,13 +19,13 @@
         <div class="scrollable-y-hover grow shrink-0 flex pl-2 lg:pl-5 pr-1 lg:pr-3">
             <!-- Sidebar Menu -->
             <x-menu class="flex flex-col grow gap-0.5">
-                <x-sidebar.menu-item>
+                <x-menu.menu-item variant="sidebar">
                     <x-sidebar.menu-label>
                         <x-sidebar.menu-icon icon="ki-home" />
                         <x-sidebar.menu-link ::class="expanded ? 'lg:block' : 'hidden'" href="{{ route('dashboard') }}"
                             :isCurrent="request()->routeIs('dashboard')">Dashboard</x-sidebar.menu-link>
                     </x-sidebar.menu-label>
-                </x-sidebar.menu-item>
+                </x-menu.menu-item>
                 <x-sidebar.menu-heading ::class="expanded ? 'lg:block' :
                     'hidden relative before:content-['...
                     '] before:absolute before:text-current before:font-before:visible before:inline-block before:bottom-2/4 before:start-0 before:ms-[.225rem] before:translate-x-full'">User</x-sidebar.menu-heading>
@@ -54,27 +54,27 @@
                             x-show="open">Stables</x-sidebar.accordian-link>
                     </x-sidebar.menu-accordian>
                 </div>
-                <x-sidebar.menu-item>
+                <x-menu.menu-item variant="sidebar">
                     <x-sidebar.menu-label>
                         <x-sidebar.menu-icon icon="ki-cup" />
                         <x-sidebar.menu-link ::class="expanded ? 'lg:block' : 'hidden'" :href="route('titles.index')"
                             :isCurrent="request()->routeIs('titles.*')">Titles</x-sidebar.menu-link>
                     </x-sidebar.menu-label>
-                </x-sidebar.menu-item>
-                <x-sidebar.menu-item>
+                </x-menu.menu-item>
+                <x-menu.menu-item variant="sidebar">
                     <x-sidebar.menu-label>
                         <x-sidebar.menu-icon icon="ki-home-3" />
                         <x-sidebar.menu-link ::class="expanded ? 'lg:block' : 'hidden'" :href="route('venues.index')"
                             :isCurrent="request()->routeIs('venues.*')">Venues</x-sidebar.menu-link>
                     </x-sidebar.menu-label>
-                </x-sidebar.menu-item>
-                <x-sidebar.menu-item>
+                </x-menu.menu-item>
+                <x-menu.menu-item variant="sidebar">
                     <x-sidebar.menu-label>
                         <x-sidebar.menu-icon icon="ki-calendar" />
                         <x-sidebar.menu-link ::class="expanded ? 'lg:block' : 'hidden'" :href="route('events.index')"
                             :isCurrent="request()->routeIs('events.*')">Events</x-sidebar.menu-link>
                     </x-sidebar.menu-label>
-                </x-sidebar.menu-item>
+                </x-menu.menu-item>
                 <x-sidebar.menu-heading ::class="expanded ? 'lg:block' :
                     'hidden relative before:content-['...
                     '] before:absolute before:text-current before:font-before:visible before:inline-block before:bottom-2/4 before:start-0 before:ms-[.225rem] before:translate-x-full'">System</x-sidebar.menu-heading>
@@ -97,13 +97,13 @@
                 <x-sidebar.menu-heading ::class="expanded ? 'lg:block' :
                     'hidden relative before:content-['...
                     '] before:absolute before:text-current before:font-before:visible before:inline-block before:bottom-2/4 before:start-0 before:ms-[.225rem] before:translate-x-full'">Docs</x-sidebar.menu-heading>
-                <x-sidebar.menu-item>
+                <x-menu.menu-item variant="sidebar">
                     <x-sidebar.menu-label>
                         <x-sidebar.menu-icon icon="ki-cup" />
                         <x-sidebar.menu-link ::class="expanded ? 'lg:block' : 'hidden'" :href="route('docs.buttons')"
                             :isCurrent="request()->routeIs('docs.buttons')">Buttons</x-sidebar.menu-link>
                     </x-sidebar.menu-label>
-                </x-sidebar.menu-item>
+                </x-menu.menu-item>
             </x-menu>
             <!-- End of Sidebar Menu -->
         </div>
