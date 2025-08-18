9d32b562491a5c8026008585aff859d4c1d7d97e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-08-17 21:01:15 -0400

    feat: convert core menu icon components from heroicons to KeenIcons
    - Update menu-icon.blade.php to use KeenIcon classes instead of heroicons
    - Simplify menu-dropdown-icon.blade.php to direct KeenIcon usage
    - Remove complex heroicon mapping logic in favor of simple icon classes
    - Maintain consistent icon sizing with text-lg and text-base classes

--- resources/views/components/menu/menu-icon.blade.php
+++ resources/views/components/menu/menu-icon.blade.php
@@ -1,21 +1,7 @@
 @props([
-    'icon' => 'squares-2x2',
+    'icon' => 'ki-element-11',
 ])
 
-@php
-    // Map ki- icons to heroicon names
-    $iconMap = [
-        'ki-home' => 'home',
-        'ki-people' => 'users', 
-        'ki-cup' => 'trophy',
-        'ki-home-3' => 'building-office',
-        'ki-calendar' => 'calendar-days',
-        'ki-element-11' => 'squares-2x2',
-    ];
-    
-    $heroIcon = $iconMap[$icon] ?? str_replace('ki-', '', $icon);
-@endphp
-
 <span {{ $attributes->merge(['class' => 'flex shrink-0']) }}>
-    <x-dynamic-component :component="'heroicon-s-' . $heroIcon" class="size-5" />
+    <i class="{{ $icon }} text-lg"></i>
 </span>
