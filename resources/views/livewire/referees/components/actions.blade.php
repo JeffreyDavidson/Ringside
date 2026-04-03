f30713ae3ef2653a1bed2adc23e4583025bd56c3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-04-03 19:56:37 -0400

    feat: rebuild action views, replace Bootstrap grid, fix raw select in tag-team modal (#586)

--- resources/views/livewire/referees/components/actions.blade.php
+++ resources/views/livewire/referees/components/actions.blade.php
@@ -1,21 +1,37 @@
 <div class="flex flex-wrap gap-2">
-    {{-- Referee Actions Component --}}
-    {{-- This component handles various referee management actions --}}
-    
-    {{-- Add action buttons here based on referee status and available actions --}}
-    {{-- Example structure for future implementation: --}}
-    {{-- 
-    @if($canEmploy)
+    @can('employ', $referee)
         <x-buttons.success wire:click="employ">{{ __('Employ') }}</x-buttons.success>
-    @endif
-    
-    @if($canRelease) 
+    @endcan
+
+    @can('release', $referee)
         <x-buttons.danger wire:click="release">{{ __('Release') }}</x-buttons.danger>
-    @endif
-    --}}
-    
-    {{-- Placeholder content for initial implementation --}}
-    <div class="text-sm text-gray-500">
-        Referee Actions Component
-    </div>
+    @endcan
+
+    @can('suspend', $referee)
+        <x-buttons.warning wire:click="suspend">{{ __('Suspend') }}</x-buttons.warning>
+    @endcan
+
+    @can('reinstate', $referee)
+        <x-buttons.success wire:click="reinstate">{{ __('Reinstate') }}</x-buttons.success>
+    @endcan
+
+    @can('injure', $referee)
+        <x-buttons.warning wire:click="injure">{{ __('Injure') }}</x-buttons.warning>
+    @endcan
+
+    @can('clearFromInjury', $referee)
+        <x-buttons.success wire:click="healFromInjury">{{ __('Heal') }}</x-buttons.success>
+    @endcan
+
+    @can('retire', $referee)
+        <x-buttons.danger wire:click="retire">{{ __('Retire') }}</x-buttons.danger>
+    @endcan
+
+    @can('unretire', $referee)
+        <x-buttons.success wire:click="unretire">{{ __('Unretire') }}</x-buttons.success>
+    @endcan
+
+    @can('restore', $referee)
+        <x-buttons.success wire:click="restore">{{ __('Restore') }}</x-buttons.success>
+    @endcan
 </div>
