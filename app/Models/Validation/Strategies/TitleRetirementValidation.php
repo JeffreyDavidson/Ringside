e52e49e43c2374871c7320e474cb1a0210226dc2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-08-10 15:13:51 -0400

    refactor: type retirement validation boundaries (#718)

--- app/Models/Validation/Strategies/TitleRetirementValidation.php
+++ app/Models/Validation/Strategies/TitleRetirementValidation.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace App\Models\Validation\Strategies;
 
 use App\Exceptions\Titles\CannotBeRetiredException;
-use App\Models\Contracts\RetirementValidationStrategy;
 use App\Models\Titles\Title;
-use Illuminate\Database\Eloquent\Model;
-use InvalidArgumentException;
 
 /**
  * Retirement validation strategy for Title entities.
@@ -22,7 +19,7 @@ use InvalidArgumentException;
  * $strategy->validate($title);
  * ```
  */
-class TitleRetirementValidation implements RetirementValidationStrategy
+class TitleRetirementValidation
 {
     /**
      * Validate that a Title can be retired.
@@ -32,15 +29,10 @@ class TitleRetirementValidation implements RetirementValidationStrategy
      * - Must not be unactivated (titles that haven't debuted can't be retired)
      * - Must not have future activation scheduled
      *
-     * @param  Model  $entity  The Title entity to validate
      * @throws CannotBeRetiredException When retirement is not allowed
      */
-    public function validate(Model $entity): void
+    public function validate(Title $entity): void
     {
-        if (! $entity instanceof Title) {
-            throw new InvalidArgumentException('TitleRetirementValidation can only validate Title entities');
-        }
-
         if ($entity->isRetired()) {
             throw CannotBeRetiredException::alreadyRetired($entity);
         }
