44852115dbf62c1aeef261b529f715e79d41eeb5
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-08-20 10:21:40 -0400

    fix: lock user updates (#1006)

--- app/Actions/Users/UpdateAction.php
+++ app/Actions/Users/UpdateAction.php
@@ -6,24 +6,32 @@ namespace App\Actions\Users;
 
 use App\Data\Users\UserData;
 use App\Models\Users\User;
+use Illuminate\Support\Facades\DB;
 
 class UpdateAction
 {
     public function handle(User $user, UserData $data): User
     {
-        $attributes = [
-            'first_name' => $data->firstName,
-            'last_name' => $data->lastName,
-            'email' => $data->email,
-            'role' => $data->role,
-        ];
+        return DB::transaction(function () use ($user, $data): User {
+            $lockedUser = User::query()
+                ->whereKey($user->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
 
-        if ($data->password !== null) {
-            $attributes['password'] = $data->password;
-        }
+            $attributes = [
+                'first_name' => $data->firstName,
+                'last_name' => $data->lastName,
+                'email' => $data->email,
+                'role' => $data->role,
+            ];
 
-        $user->update($attributes);
+            if ($data->password !== null) {
+                $attributes['password'] = $data->password;
+            }
 
-        return $user;
+            $lockedUser->update($attributes);
+
+            return $lockedUser;
+        });
     }
 }
