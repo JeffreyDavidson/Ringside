119c3b4d0d2d4baa638d4c401cdb266bebc3be0e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-08-22 17:17:31 -0400

    fix: add title type selector (#1090)

--- resources/views/livewire/titles/modals/form-modal.blade.php
+++ resources/views/livewire/titles/modals/form-modal.blade.php
@@ -4,6 +4,10 @@
     </x-form-modal.modal-input>
 
     <x-form-modal.modal-input>
+        <x-form.inputs.select label="{{ __('titles.type') }}" wire:model="form.type" :options="$this->getTitleTypes" />
+    </x-form-modal.modal-input>
+
+    <x-form-modal.modal-input>
         <x-form.inputs.date label="{{ __('activations.started_at') }}" wire:model="form.start_date" />
     </x-form-modal.modal-input>
 </x-form-modal>
