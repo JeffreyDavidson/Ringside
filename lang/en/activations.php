1e30574bad7325e05d9a1a733f43af7f0aca86aa
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-08-22 16:39:30 -0400

    fix: correct stable form field bindings (#1089)

--- lang/en/activations.php
+++ lang/en/activations.php
@@ -2,6 +2,7 @@
 
 return [
     'started_at' => 'Activation Date',
+    'ended_at' => 'Deactivation Date',
 
     'validation' => [
         'activation_active' => ':name is currently activated and the activation date cannot be changed.',
