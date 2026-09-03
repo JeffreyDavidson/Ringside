b39ad90a4c77467a2c5647829c6a48d2dc115167
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-03 11:34:50 -0400

    test: consolidate authentication workflows (#1290)

--- tests/Feature/Http/Controllers/Auth/AuthenticationTest.php
+++ tests/Feature/Http/Controllers/Auth/AuthenticationTest.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 use App\Models\Users\User;
 use App\Providers\AppServiceProvider;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertAuthenticated;
 use function Pest\Laravel\assertGuest;
+use function Pest\Laravel\from;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 test('login screen can be rendered', function () {
     // Arrange
     $loginUrl = route('login');
 
     // Act
-    $response = $this->get($loginUrl);
+    $response = get($loginUrl);
 
     // Assert
     $response->assertSuccessful();
@@ -28,7 +32,7 @@ test('users can authenticate using the login screen', function () {
     ];
 
     // Act
-    $response = $this->post(route('login'), $credentials);
+    $response = post(route('login'), $credentials);
 
     // Assert
     $response->assertRedirect(AppServiceProvider::HOME);
@@ -44,11 +48,35 @@ test('users can not authenticate with invalid password', function () {
     ];
 
     // Act
-    $response = $this->from(route('login'))->post(route('login'), $credentials);
+    $response = from(route('login'))->post(route('login'), $credentials);
 
     // Assert
     $response
         ->assertRedirect(route('login'))
         ->assertSessionHasErrors('email');
+    assertGuest();
+});
+
+test('login requires credentials', function () {
+    // Arrange
+    $credentials = [];
+
+    // Act
+    $response = post(route('login'), $credentials);
+
+    // Assert
+    $response->assertSessionHasErrors(['email', 'password']);
+    assertGuest();
+});
+
+test('authenticated users can log out', function () {
+    // Arrange
+    actingAs(administrator());
+
+    // Act
+    $response = post(route('logout'));
+
+    // Assert
+    $response->assertRedirect(route('login'));
     assertGuest();
 });
