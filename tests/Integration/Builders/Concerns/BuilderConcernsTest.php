b4854be5a2ae9b2ccda7b9f6517ab7bdeb4829be
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-07 21:17:38 -0400

    test: protect roster employment filtering

--- tests/Integration/Builders/Concerns/BuilderConcernsTest.php
+++ tests/Integration/Builders/Concerns/BuilderConcernsTest.php
@@ -25,51 +25,135 @@ test('employment statuses map to their shared roster query constraints', functio
 });
 
 test('wrestlers may be filtered by employment status', function (): void {
+    // Arrange
     $employed = Wrestler::factory()->employed()->create();
     $unemployed = Wrestler::factory()->unemployed()->create();
     $released = Wrestler::factory()->released()->create();
     $futureEmployed = Wrestler::factory()->withFutureEmployment()->create();
+    Wrestler::factory()->retired()->create();
+    Wrestler::factory()->employed()->trashed()->create();
+    Wrestler::factory()->unemployed()->trashed()->create();
+    Wrestler::factory()->released()->trashed()->create();
+    Wrestler::factory()->withFutureEmployment()->trashed()->create();
 
-    expect(Wrestler::query()->employed()->pluck('id')->all())->toBe([$employed->id])
-        ->and(Wrestler::query()->unemployed()->pluck('id')->all())->toBe([$unemployed->id])
-        ->and(Wrestler::query()->released()->pluck('id')->all())->toBe([$released->id])
-        ->and(Wrestler::query()->futureEmployed()->pluck('id')->all())->toBe([$futureEmployed->id]);
+    // Act
+    $employedQuery = Wrestler::query();
+    $employedQuery->employed();
+    $employedResults = $employedQuery->get();
+    $unemployedQuery = Wrestler::query();
+    $unemployedQuery->unemployed();
+    $unemployedResults = $unemployedQuery->get();
+    $releasedQuery = Wrestler::query();
+    $releasedQuery->released();
+    $releasedResults = $releasedQuery->get();
+    $futureEmployedQuery = Wrestler::query();
+    $futureEmployedQuery->futureEmployed();
+    $futureEmployedResults = $futureEmployedQuery->get();
+
+    // Assert
+    expect($employedResults->modelKeys())->toBe([$employed->id])
+        ->and($unemployedResults->modelKeys())->toBe([$unemployed->id])
+        ->and($releasedResults->modelKeys())->toBe([$released->id])
+        ->and($futureEmployedResults->modelKeys())->toBe([$futureEmployed->id]);
 });
 
 test('managers may be filtered by employment status', function (): void {
+    // Arrange
     $employed = Manager::factory()->employed()->create();
     $unemployed = Manager::factory()->unemployed()->create();
     $released = Manager::factory()->released()->create();
     $futureEmployed = Manager::factory()->withFutureEmployment()->create();
+    Manager::factory()->retired()->create();
+    Manager::factory()->employed()->trashed()->create();
+    Manager::factory()->unemployed()->trashed()->create();
+    Manager::factory()->released()->trashed()->create();
+    Manager::factory()->withFutureEmployment()->trashed()->create();
 
-    expect(Manager::query()->employed()->pluck('id')->all())->toBe([$employed->id])
-        ->and(Manager::query()->unemployed()->pluck('id')->all())->toBe([$unemployed->id])
-        ->and(Manager::query()->released()->pluck('id')->all())->toBe([$released->id])
-        ->and(Manager::query()->futureEmployed()->pluck('id')->all())->toBe([$futureEmployed->id]);
+    // Act
+    $employedQuery = Manager::query();
+    $employedQuery->employed();
+    $employedResults = $employedQuery->get();
+    $unemployedQuery = Manager::query();
+    $unemployedQuery->unemployed();
+    $unemployedResults = $unemployedQuery->get();
+    $releasedQuery = Manager::query();
+    $releasedQuery->released();
+    $releasedResults = $releasedQuery->get();
+    $futureEmployedQuery = Manager::query();
+    $futureEmployedQuery->futureEmployed();
+    $futureEmployedResults = $futureEmployedQuery->get();
+
+    // Assert
+    expect($employedResults->modelKeys())->toBe([$employed->id])
+        ->and($unemployedResults->modelKeys())->toBe([$unemployed->id])
+        ->and($releasedResults->modelKeys())->toBe([$released->id])
+        ->and($futureEmployedResults->modelKeys())->toBe([$futureEmployed->id]);
 });
 
 test('referees may be filtered by employment status', function (): void {
+    // Arrange
     $employed = Referee::factory()->employed()->create();
     $unemployed = Referee::factory()->unemployed()->create();
     $released = Referee::factory()->released()->create();
     $futureEmployed = Referee::factory()->withFutureEmployment()->create();
+    Referee::factory()->retired()->create();
+    Referee::factory()->employed()->trashed()->create();
+    Referee::factory()->unemployed()->trashed()->create();
+    Referee::factory()->released()->trashed()->create();
+    Referee::factory()->withFutureEmployment()->trashed()->create();
 
-    expect(Referee::query()->employed()->pluck('id')->all())->toBe([$employed->id])
-        ->and(Referee::query()->unemployed()->pluck('id')->all())->toBe([$unemployed->id])
-        ->and(Referee::query()->released()->pluck('id')->all())->toBe([$released->id])
-        ->and(Referee::query()->futureEmployed()->pluck('id')->all())->toBe([$futureEmployed->id]);
+    // Act
+    $employedQuery = Referee::query();
+    $employedQuery->employed();
+    $employedResults = $employedQuery->get();
+    $unemployedQuery = Referee::query();
+    $unemployedQuery->unemployed();
+    $unemployedResults = $unemployedQuery->get();
+    $releasedQuery = Referee::query();
+    $releasedQuery->released();
+    $releasedResults = $releasedQuery->get();
+    $futureEmployedQuery = Referee::query();
+    $futureEmployedQuery->futureEmployed();
+    $futureEmployedResults = $futureEmployedQuery->get();
+
+    // Assert
+    expect($employedResults->modelKeys())->toBe([$employed->id])
+        ->and($unemployedResults->modelKeys())->toBe([$unemployed->id])
+        ->and($releasedResults->modelKeys())->toBe([$released->id])
+        ->and($futureEmployedResults->modelKeys())->toBe([$futureEmployed->id]);
 });
 
 test('tag teams may be filtered by employment status', function (): void {
+    // Arrange
     $employed = TagTeam::factory()->employed()->create();
     $unemployed = TagTeam::factory()->unemployed()->create();
     $released = TagTeam::factory()->released()->create();
     $futureEmployed = TagTeam::factory()->withFutureEmployment()->create();
+    TagTeam::factory()->retired()->create();
+    TagTeam::factory()->employed()->trashed()->create();
+    TagTeam::factory()->unemployed()->trashed()->create();
+    TagTeam::factory()->released()->trashed()->create();
+    TagTeam::factory()->withFutureEmployment()->trashed()->create();
 
-    expect(TagTeam::query()->employed()->pluck('id')->all())->toBe([$employed->id])
-        ->and(TagTeam::query()->unemployed()->pluck('id')->all())->toBe([$unemployed->id])
-        ->and(TagTeam::query()->released()->pluck('id')->all())->toBe([$released->id])
-        ->and(TagTeam::query()->futureEmployed()->pluck('id')->all())->toBe([$futureEmployed->id]);
+    // Act
+    $employedQuery = TagTeam::query();
+    $employedQuery->employed();
+    $employedResults = $employedQuery->get();
+    $unemployedQuery = TagTeam::query();
+    $unemployedQuery->unemployed();
+    $unemployedResults = $unemployedQuery->get();
+    $releasedQuery = TagTeam::query();
+    $releasedQuery->released();
+    $releasedResults = $releasedQuery->get();
+    $futureEmployedQuery = TagTeam::query();
+    $futureEmployedQuery->futureEmployed();
+    $futureEmployedResults = $futureEmployedQuery->get();
+
+    // Assert
+    expect($employedResults->modelKeys())->toBe([$employed->id])
+        ->and($unemployedResults->modelKeys())->toBe([$unemployed->id])
+        ->and($releasedResults->modelKeys())->toBe([$released->id])
+        ->and($futureEmployedResults->modelKeys())->toBe([$futureEmployed->id]);
 });
 
 test('wrestlers may be filtered by retirement status', function (): void {
