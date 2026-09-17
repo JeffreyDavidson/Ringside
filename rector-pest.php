b1ab964c0cb514c7e02ac1dd7a0b354d53ba758a
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-17 14:19:29 -0400

    fix: preserve existing Rector behavior

--- rector-pest.php
+++ rector-pest.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use Pest\Rector\Set\PestSetList;
+use Rector\CodeQuality\Rector\If_\ObjectExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\If_\IfToNullCoalescingAssignRector;
+use Rector\TypeDeclaration\Rector\BooleanAnd\BinaryOpNullableToInstanceofRector;
+use RectorLaravel\Rector\Class_\LivewireComponentComputedMethodToComputedAttributeRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,4 +17,10 @@ return RectorConfig::configure()
     ->withSets([
         PestSetList::CODING_STYLE,
     ])
-    ->withComposerBased(laravel: true);
+    ->withComposerBased(laravel: true)
+    ->withSkip([
+        BinaryOpNullableToInstanceofRector::class,
+        IfToNullCoalescingAssignRector::class,
+        LivewireComponentComputedMethodToComputedAttributeRector::class,
+        ObjectExplicitBoolCompareRector::class,
+    ]);
