6a696e34fb4da808c9a9b0ccf274b25be89092f9
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-19 19:44:44 -0400

    fix: complete password reset flow

--- tests/Feature/Http/Controllers/Auth/PasswordResetLinkControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/PasswordResetLinkControllerTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use App\Models\Users\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
 
 test('password reset link screen can be rendered', function () {
     // Arrange
@@ -44,4 +46,40 @@ test('password reset link can be requested', function () {
     // Assert
     $response->assertSessionHasNoErrors();
     Notification::assertSentTo($user, ResetPassword::class);
+});
+
+test('password reset form can be rendered', function () {
+    // Arrange
+    $user = User::factory()->create();
+    $token = Password::broker()->createToken($user);
+
+    // Act
+    $response = $this->get(route('password.reset', [
+        'token' => $token,
+        'email' => $user->email,
+    ]));
+
+    // Assert
+    $response->assertSuccessful();
+    $response->assertViewIs('auth.passwords.reset');
+});
+
+test('password can be reset with a valid token', function () {
+    // Arrange
+    $user = User::factory()->create();
+    $token = Password::broker()->createToken($user);
+    $requestData = [
+        'token' => $token,
+        'email' => $user->email,
+        'password' => 'new-password',
+        'password_confirmation' => 'new-password',
+    ];
+
+    // Act
+    $response = $this->post(route('password.update'), $requestData);
+
+    // Assert
+    $response->assertRedirect(route('login'));
+    $response->assertSessionHasNoErrors();
+    expect(Hash::check('new-password', $user->refresh()->password))->toBeTrue();
 });
