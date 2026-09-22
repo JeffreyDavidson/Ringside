927fac20827e7df5248463bbde73846973ee6ee3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-22 18:25:35 -0400

    fix: slow sidebar collapse animation

--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -49,7 +49,7 @@
                 class="border-ringside-line bg-ringside-surface-header text-ringside-muted hover:bg-ringside-surface-hover hover:text-ringside-ink focus-visible:outline-ringside-ink absolute end-0 top-1/2 hidden size-8 translate-x-1/2 -translate-y-1/2 items-center justify-center border focus-visible:outline-2 focus-visible:outline-offset-4 lg:inline-flex"
             >
                 <x-heroicon-s-chevron-left
-                    class="size-4 origin-center transform-gpu transition-[rotate] duration-[var(--sidebar-transition-duration)] ease-in-out"
+                    class="size-4 origin-center transform-gpu transition-all duration-[var(--sidebar-transition-duration)] ease-in-out"
                     x-bind:class="expanded ? 'rotate-0' : 'rotate-180'"
                 />
             </button>
