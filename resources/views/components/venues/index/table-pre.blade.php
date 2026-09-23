7836d5a98f7779e50da8b23a73cbe07c97436b6b
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-22 23:14:30 -0400

    feat: migrate venues index to workspace shell (#1632)

--- resources/views/components/venues/index/table-pre.blade.php
+++ resources/views/components/venues/index/table-pre.blade.php
@@ -1,8 +1,14 @@
-<x-layouts.table-header title="Venues">
+<x-layouts.table-header title="Venues" :subtitle="__('venues.index_description')">
     <x-slot:actions>
         @can('create', \App\Models\Events\Venue::class)
-            <x-buttons.primary size="sm" @click="$dispatch('openModal', { component: 'venues.modals.form-modal' })">
-                Add Venue</x-buttons.primary>
+            <x-button
+                variant="ringside"
+                size="md"
+                class="min-h-11"
+                @click="$dispatch('openModal', { component: 'venues.modals.form-modal' })"
+            >
+                Add Venue
+            </x-button>
         @endcan
     </x-slot:actions>
 </x-layouts.table-header>
--- resources/views/venues/index.blade.php
+++ resources/views/venues/index.blade.php
@@ -1,3 +1,5 @@
 <x-layouts.app>
-    <livewire:venues.tables.main />
+    <x-layouts.workspace-canvas class="flex flex-col gap-5 p-4 lg:p-7">
+        <livewire:venues.tables.main />
+    </x-layouts.workspace-canvas>
 </x-layouts.app>
