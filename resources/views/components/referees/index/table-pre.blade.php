bad2c5e2db9f63205dd7a016597d87b2f207d374
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-22 21:15:35 -0400

    feat: migrate referees page to workspace shell

--- resources/views/components/referees/index/table-pre.blade.php
+++ resources/views/components/referees/index/table-pre.blade.php
@@ -1,8 +1,14 @@
-<x-layouts.table-header title="Referees">
+<x-layouts.table-header title="Referees" :subtitle="__('referees.index_description')">
     <x-slot:actions>
         @can('create', \App\Models\Roster\Referees\Referee::class)
-            <x-buttons.primary size="sm" @click="$dispatch('openModal', { component: 'referees.modals.form-modal' })">
-                Add Referee</x-buttons.primary>
+            <x-button
+                variant="ringside"
+                size="md"
+                class="min-h-11"
+                @click="$dispatch('openModal', { component: 'referees.modals.form-modal' })"
+            >
+                Add Referee
+            </x-button>
         @endcan
     </x-slot:actions>
 </x-layouts.table-header>
