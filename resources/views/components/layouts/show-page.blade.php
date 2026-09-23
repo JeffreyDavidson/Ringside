2e9dc1d66cb024d26e98d1d8cd7ead0626610d8f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-23 09:28:12 -0400

    feat: migrate detail pages to workspace shell

--- resources/views/components/layouts/show-page.blade.php
+++ resources/views/components/layouts/show-page.blade.php
@@ -1,20 +1,22 @@
 @props(['sidebar', 'title' => null])
 
 <x-layouts.app>
-    <x-container-fluid>
+    <x-layouts.workspace-canvas class="flex flex-col gap-5 p-4 lg:gap-7.5 lg:p-7">
         @if ($title)
-            <div class="mb-5 lg:mb-7.5">
-                <h1 class="text-lg font-semibold text-gray-900">{{ $title }}</h1>
+            <div>
+                <h1 class="font-display text-ringside-ink text-2xl leading-none tracking-tight uppercase">
+                    {{ $title }}
+                </h1>
             </div>
         @endif
 
         <div class="grid grid-cols-1 gap-5 lg:grid-cols-3 lg:gap-7.5">
-            <div class="col-span-1">
+            <div class="col-span-1 min-w-0">
                 <div class="grid gap-5 lg:gap-7.5">{{ $sidebar }}</div>
             </div>
-            <div class="col-span-2">
+            <div class="col-span-1 min-w-0 lg:col-span-2">
                 <div class="flex flex-col gap-5 lg:gap-7.5">{{ $slot }}</div>
             </div>
         </div>
-    </x-container-fluid>
+    </x-layouts.workspace-canvas>
 </x-layouts.app>
