4a6906c2ec88f77b1a6724bdd7ef254c8fa899f2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-22 22:50:06 -0400

    feat: finish events index workspace migration (#1631)

--- resources/views/components/events/index/table-pre.blade.php
+++ resources/views/components/events/index/table-pre.blade.php
@@ -1,9 +1,10 @@
-<x-layouts.table-header title="Events">
+<x-layouts.table-header title="Events" :subtitle="__('events.index_description')">
     <x-slot:actions>
         @can('create', \App\Models\Events\Event::class)
             <x-button
                 variant="ringside"
                 size="md"
+                class="min-h-11"
                 @click="$dispatch('openModal', { component: 'events.modals.form-modal' })"
             >
                 Add Event
