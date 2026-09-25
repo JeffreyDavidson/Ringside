2e9afe9544c8b5233bb931aef950d22f92213f20
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-24 20:17:12 -0400

    fix(events): improve responsive event form layout (#1660)

--- resources/views/livewire/events/modals/form-modal.blade.php
+++ resources/views/livewire/events/modals/form-modal.blade.php
@@ -3,18 +3,24 @@
         <x-form.inputs.text label="{{ __('events.name') }}" wire:model="form.name" />
     </x-form-modal.modal-input>
 
-    <x-form-modal.modal-input>
-        <x-form.input
-            type="datetime-local"
-            label="{{ __('events.date_time') }}"
-            wire:model="form.date"
-            class="event-date-time [color-scheme:dark]"
-        />
-    </x-form-modal.modal-input>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" data-test="event-schedule-grid">
+        <x-form-modal.modal-input>
+            <x-form.input
+                type="datetime-local"
+                label="{{ __('events.date_time') }}"
+                wire:model="form.date"
+                class="event-date-time [color-scheme:dark]"
+            />
+        </x-form-modal.modal-input>
 
-    <x-form-modal.modal-input>
-        <x-form.inputs.select label="{{ __('events.venue') }}" wire:model="form.venue_id" :options="$this->getVenues" />
-    </x-form-modal.modal-input>
+        <x-form-modal.modal-input>
+            <x-form.inputs.select
+                label="{{ __('events.venue') }}"
+                wire:model="form.venue_id"
+                :options="$this->getVenues"
+            />
+        </x-form-modal.modal-input>
+    </div>
 
     <x-form-modal.modal-input>
         <x-form.inputs.textarea label="{{ __('events.preview') }}" wire:model="form.preview" />
