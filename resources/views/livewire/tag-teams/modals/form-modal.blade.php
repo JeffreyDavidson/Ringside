dbdbfb9e24609cf2502bbafe73721c6084538368
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-24 21:35:57 -0400

    fix: improve tag-team form responsiveness (#1662)

--- resources/views/livewire/tag-teams/modals/form-modal.blade.php
+++ resources/views/livewire/tag-teams/modals/form-modal.blade.php
@@ -1,40 +1,46 @@
 <x-form-modal>
-    <x-form-modal.modal-input>
-        <x-form.inputs.text label="{{ __('tag-teams.name') }}" wire:model="form.name" />
-    </x-form-modal.modal-input>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" data-test="tag-team-details-grid">
+        <x-form-modal.modal-input>
+            <x-form.inputs.text label="{{ __('tag-teams.name') }}" wire:model="form.name" />
+        </x-form-modal.modal-input>
 
-    <x-form-modal.modal-input>
-        <x-form.inputs.text label="{{ __('tag-teams.signature_move') }}" wire:model="form.signature_move" />
-    </x-form-modal.modal-input>
+        <x-form-modal.modal-input>
+            <x-form.inputs.text label="{{ __('tag-teams.signature_move') }}" wire:model="form.signature_move" />
+        </x-form-modal.modal-input>
+    </div>
+
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" data-test="tag-team-wrestlers-grid">
+        <x-form-modal.modal-input>
+            <x-form.inputs.select
+                label="{{ __('tag-teams.wrestlerA') }}"
+                wire:model="form.wrestlerA"
+                :options="$this->getWrestlers"
+                selected="form.wrestlerA"
+            />
+        </x-form-modal.modal-input>
+
+        <x-form-modal.modal-input>
+            <x-form.inputs.select
+                label="{{ __('tag-teams.wrestlerB') }}"
+                wire:model="form.wrestlerB"
+                :options="$this->getWrestlers"
+                selected="form.wrestlerB"
+            />
+        </x-form-modal.modal-input>
+    </div>
 
     <x-form-modal.modal-input>
         <x-form.inputs.date label="{{ __('employments.started_at') }}" wire:model="form.employment_date" />
     </x-form-modal.modal-input>
 
-    <x-form-modal.modal-input>
-        <x-form.inputs.select
-            label="{{ __('tag-teams.wrestlerA') }}"
-            wire:model="form.wrestlerA"
-            :options="$this->getWrestlers"
-            selected="form.wrestlerA"
-        />
-    </x-form-modal.modal-input>
-
-    <x-form-modal.modal-input>
-        <x-form.inputs.select
-            label="{{ __('tag-teams.wrestlerB') }}"
-            wire:model="form.wrestlerB"
-            :options="$this->getWrestlers"
-            selected="form.wrestlerB"
-        />
-    </x-form-modal.modal-input>
-
-    <x-form-modal.modal-input>
-        <x-form.inputs.select
-            label="{{ __('tag-teams.managers') }}"
-            wire:model="form.managers"
-            :options="$this->getManagers"
-            :multiple="true"
-        />
-    </x-form-modal.modal-input>
+    <div data-test="tag-team-managers-field">
+        <x-form-modal.modal-input>
+            <x-form.inputs.select
+                label="{{ __('tag-teams.managers') }}"
+                wire:model="form.managers"
+                :options="$this->getManagers"
+                :multiple="true"
+            />
+        </x-form-modal.modal-input>
+    </div>
 </x-form-modal>
